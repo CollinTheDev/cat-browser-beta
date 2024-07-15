096748ef16f0b243b396db28881f641d1d0d0cfb
CollinTheDev <collin.entringer@gmail.com>
2024-07-15 16:40:58 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAOK2H7XklP-_ng407mj___hy3Fy9InNtk",
+    authDomain: "bettercord-e4e7d.firebaseapp.com",
+    projectId: "bettercord-e4e7d",
+    storageBucket: "bettercord-e4e7d.appspot.com",
+    messagingSenderId: "934430756781",
+    appId: "1:934430756781:web:faedd45b79251803721b24"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+var db = firebase.firestore();
